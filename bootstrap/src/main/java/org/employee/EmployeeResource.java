8c602f3a094c72ef7b59ebcf9a33c7838892b7a1
sheetal2211 <sheetal.agarwal2211@gmail.com>
2024-08-04 19:50:24 +0530

    feat: add post and get method logic

--- bootstrap/src/main/java/org/employee/EmployeeResource.java
+++ bootstrap/src/main/java/org/employee/EmployeeResource.java
@@ -2,13 +2,10 @@ package org.employee;
 
 import org.employee.model.Employee;
 import org.employee.model.Employees;
-import org.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
